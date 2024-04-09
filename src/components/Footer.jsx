612644453610bfba4b3245414374e45538b74f98
Benjamin Hutchings <benjamin.a.hutchings@gmail.com>
2024-04-09 21:08:34 +0100

    update packages and href in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,18 @@ const Footer = () => {
           <span className="ml-3 text-xl">Benjamin Hutchings</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {new Date().getFullYear()} Your Brand —
-          <a href="https://twitter.com/yourusername" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">ADD SOCIALS HERE</a>
+          <a href="#contact" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">ADD SOCIALS HERE</a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a href="https://facebook.com" className="text-gray-500">
+          <a href="#contact" className="text-gray-500">
             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
               {/* Facebook icon SVG */}
             </svg>
           </a>
-          <a href="https://twitter.com" className="ml-3 text-gray-500">
+          <a href="#contactm" className="ml-3 text-gray-500">
             {/* Twitter icon SVG */}
           </a>
-          <a href="https://instagram.com" className="ml-3 text-gray-500">
+          <a href="#contact" className="ml-3 text-gray-500">
             {/* Instagram icon SVG */}
           </a>
         </span>
