2242cd260980b9ef72d391cec6c6d07ffc03d5dc
Benjamin Hutchings <benjamin.a.hutchings@gmail.com>
2024-06-01 16:46:24 +0100

    Added Leetcode and Website as Projects
    Also added colour to project titles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,6 +51,27 @@ const Projects = () => {
     },
   };
 
+  const getColor = (index) => {
+    const colors = [
+      "#d9e8f5", // Soft Blue
+      "#d5e8d4", // Muted Green
+      "#fff9db", // Pale Yellow
+      "#f5e8d9", // Warm Beige
+    ];
+    return colors[index % colors.length];
+  };
+
+  const getColorAdditional = (index) => {
+    const colors = [
+      "#fad4d4", // Light Coral
+      "#e8d9f5", // Lavender
+      "#d9f5f3", // Light Cyan
+      "#fffacd", // Lemon Chiffon
+      "#ffe4e1", // Misty Rose
+    ];
+    return colors[index % colors.length];
+  };
+
   const featuredProjects = [
     {
       category: "Deep Learning Frameworks",
@@ -84,76 +105,113 @@ const Projects = () => {
     {
       title: "LeetCode Submissions",
       category: "Algorithms and Data Structures",
-      description: "A collection of solutions to algorithm and data structure problems on LeetCode.",
+      description: "A collection of my solutions for algorithm and data structure problems on LeetCode with implementations in Python.",
+      imageUrl: "img/project_img_leetcode.webp",
       githubUrl: "https://github.com/benjamin-hutchings/LeetCode-Submissions",
       liveDemoUrl: "https://github.com/benjamin-hutchings/LeetCode-Submissions",
-      hashtags: ["Python", "Algorithms", "Data Structures", "LeetCode"],
+      hashtags: ["Python", "LeetCode"],
     },
     {
-      title: "Project 5",
-      category: "Category",
-      description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      title: "This Website!",
+      category: "Portfolio Webpage",
+      description: "A fully responsive and modern portfolio website built with React, TailwindCSS and ThreeJS.",
+      imageUrl: "img/project_img_website.webp",
+      githubUrl: "https://github.com/benjamin-hutchings/portfolio-website",
+      liveDemoUrl: "http://localhost:3000/#projects",
+      hashtags: ["React", "TailwindCSS", "JavaScript", "ThreeJS"],
     },
     {
       title: "Project 6",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-6",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-6",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 7",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-7",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-7",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 8",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-8",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-8",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 9",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-9",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-9",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 10",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-10",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-10",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 11",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-11",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-11",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
     {
       title: "Project 12",
       category: "Category",
       description: "Project description...",
-      githubUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      liveDemoUrl: "https://elegant-pasca-f13e12.netlify.app/",
-      hashtags: ["Python", "Data"],
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-12",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-12",
+      hashtags: ["Hashtag1", "Hashtag2"],
+    },
+    {
+      title: "Project 13",
+      category: "Category",
+      description: "Project description...",
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-13",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-13",
+      hashtags: ["Hashtag1", "Hashtag2"],
+    },
+    {
+      title: "Project 14",
+      category: "Category",
+      description: "Project description...",
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-14",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-14",
+      hashtags: ["Hashtag1", "Hashtag2"],
+    },
+    {
+      title: "Project 15",
+      category: "Category",
+      description: "Project description...",
+      imageUrl: "img/github-logo.png",
+      githubUrl: "https://github.com/benjamin-hutchings/project-15",
+      liveDemoUrl: "https://github.com/benjamin-hutchings/project-15",
+      hashtags: ["Hashtag1", "Hashtag2"],
     },
   ];
+
   return (
     <section
       id="projects"
@@ -165,8 +223,8 @@ const Projects = () => {
             Projects
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Here are some side-projects I've worked on as part of my personal
-            portfolio...
+            Here are some side-projects I've put together as part of my personal
+            portfolio.
           </p>
         </div>
 
@@ -178,10 +236,11 @@ const Projects = () => {
           className="flex flex-wrap -m-4 items-center"
         >
           {featuredProjects.map((project, index) => (
-            <div key={index} className="p-4 flex-col mx-auto sm:w-full md:w-1/2 lg:w-1/3 ">
+            <div key={index} className="p-4 flex-col mx-auto sm:w-full md:w-1/2 lg:w-1/3">
               <motion.div
                 whileHover={hoverEffect}
-                className="flex flex-col justify-between bg-gray-100 bg-opacity-75 p-8 rounded-lg overflow-hidden text-center relative min-h-[500px]"
+                className="flex flex-col justify-between p-8 rounded-lg overflow-hidden text-center relative min-h-[500px]"
+                style={{ backgroundColor: getColor(index) }}
               >
                 <img
                   src={project.imageUrl}
@@ -214,7 +273,9 @@ const Projects = () => {
                   </a>
                 </div>
                 <p className="text-xs font-semibold text-indigo-500">
-                  #{project.hashtags.join(" #")}
+                  {project.hashtags.map((tag, i) => (
+                    <span key={i}>#{tag} </span>
+                  ))}
                 </p>
               </motion.div>
             </div>
@@ -224,39 +285,54 @@ const Projects = () => {
           {additionalProjects.map((project, index) => (
             <motion.div
               key={index}
-              className="min-w-[300px] p-4 bg-gray-100 rounded-lg"
+              className="min-w-[300px] p-4 flex flex-col justify-between rounded-lg"
               initial="hidden"
               animate={controls}
               variants={fadeInVariant}
               whileHover={hoverEffect}
+              style={{ backgroundColor: getColorAdditional(index) }}
             >
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                {project.category}
-              </h2>
-              <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
-                {project.title}
-              </h1>
-              <div className="flex justify-center gap-4 mb-3">
-                <a
-                  href={project.githubUrl}
-                  className="bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700 transition-colors"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  GitHub
-                </a>
-                <a
-                  href={project.liveDemoUrl}
-                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-400 transition-colors"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Live Demo
-                </a>
+              <img
+                src={project.imageUrl}
+                alt="Project Thumbnail"
+                className="w-full h-32 object-cover object-center rounded-lg mb-4"
+              />
+              <div className="flex flex-col flex-grow justify-between">
+                <div>
+                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                    {project.category}
+                  </h2>
+                  <h1 className="title-font text-xl font-medium text-gray-900 mb-2">
+                    {project.title}
+                  </h1>
+                  <p className="mb-4">{project.description}</p>
+                </div>
+                <div>
+                  <div className="flex justify-center gap-4 mb-3">
+                    <a
+                      href={project.githubUrl}
+                      className="bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700 transition-colors"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      GitHub
+                    </a>
+                    <a
+                      href={project.liveDemoUrl}
+                      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-400 transition-colors"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
+                  <p className="text-xs font-semibold text-indigo-500 text-right">
+                    {project.hashtags.map((tag, i) => (
+                      <span key={i}>#{tag} </span>
+                    ))}
+                  </p>
+                </div>
               </div>
-              <p className="text-xs font-semibold text-indigo-500 text-right">
-                #{project.hashtags.join(" #")}
-              </p>
             </motion.div>
           ))}
         </div>
